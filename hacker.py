c0ffc407861adc7e584b5eee8c8c6ae666bbe66c
taufiqx404 <59577411+taufiqx404@users.noreply.github.com>
2020-08-06 05:37:09 +0800

    Update and rename README.md to hacker.py

---
+++ hacker.py
@@ -0,0 +1,71 @@
+import os
+import sys
+import time
+
+os.system('clear')
+time.sleep(1)
+os.system('figlet WELCOME TO MY WORLD')
+print "=========================================="
+print "             RED DEVIL OFF                "
+print " TAUFIQ X ,RISKY,LUCKY,BANDIT,PANGLIMA"
+print "=========================================="
+print " AUTHOR : TAUFIQ X "
+print " TEAM   : RED DEVIL OFF"
+print " GITHUB :taufiqx404 "
+print "=============================="
+print
+time.sleep(3)
+print "[+] MENU PILIHAN NYA TOD[+]"
+print "[1] DARK FBV1.7"
+print "[2] DARK FBV1.8"
+print "[3] DARK FB VIP"
+print "[4] DARK FB2.0"
+print "[5] CREATE VIRUS"
+print "[6]INSTALL BAHAN TOL"
+time.sleep(5)
+pilih = raw_input('[?] pilih :')
+if pilih =="1":
+       os.system(' git clone https://github.com/B4N954N2-ID/DARK')
+       time.sleep(1)
+       print "[+]SELESAI MENGINSTALL"
+       time.sleep(2)
+       print "[+]KETIK cd DARK"
+       time.sleep(2)
+       print "[+]next KETIK python2 darkfb.py"
+elif pilih == "2":
+       os.system('git clone htpps://github.com/B4N954N2-ID/DarkFb')
+       time.sleep(1)
+       print "[+]SELESAI MENGINSTALL"
+       time.sleep(1)
+       print "[+]KETIK cd DarkFb"
+       time.sleep(1)
+       print "[+]next KETIK python2 DarkFb.py"
+elif pilih == "3":
+       os.system('git clone https://github.com/B4N954N2-ID/Darkpremium')
+       time.sleep(1)
+       print "[+]SELESAI MENGINSTALL"
+       time.sleep(1)
+       print "[+]KETIK cd Darkpremium"
+       time.sleep(1)
+       print "[+]next KETIK python2 darkpremium.py"
+elif pilih == "4":
+       os.system('git clone https://github.com/B4N954N2-ID/dark-fb')
+       time.sleep(1)
+       print "[+]SELESAI MENGINSTALL"
+       time.sleep(1)
+       print "[+]KETIK cd dark-fb"
+       time.sleep(1)
+       print "[+] next KETIK python2 darkfb.py"
+elif pilih == "5":
+               os.system('git clone https://github.com/saydog/Virus-droid-creator ')
+         print "[+]SELESAI MENGINSTALL"  
+         print "[+]KETIK cd Virus-droid-creator"
+         print "[+]KETIK python2 dog.py"
+elif pilih =="6":
+       os.system('apt update && apt upgrade')
+       os.system('pkg install python2')
+       os.system('pkg install nano')
+       os.system('pkg install git')
+       os.system('pip2 install requests mechanize')
+       time.sleep(1)
+       print "[+]  SELESAI MENGINSTALL "
